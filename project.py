6201b712bb661c007347b265e032b5b93327a0cc
U170709010 <misraturgut@gmail.com>
2021-01-21 22:20:02 +0300

    I set up random state, letter holding array, shear values then created a func that generates samples, returns the image of letters then I called this func to generate new sample

--- project.py
+++ project.py
@@ -44,5 +44,29 @@ for i in range(len(subimages)):
     axes[i].imshow(subimages[i], cmap="gray")
 
 
+from sklearn.utils import check_random_state
+random_state = check_random_state(14)
+letters = list("ABCDEFGHIJKLMNOPRSTUVWXYZ")
+shear_values = np.arange(0, 0.5, 0.05)
+
+
+def generate_sample(random_state=None):
+    random_state=check_random_state(random_state)
+    letter = random_state.choice(letters)
+    shear = random_state.choice(shear_values)
+
+
+    return create_captcha(letter, shear=shear, size=(20,20)),letter.index(letter)
+
+
+image, target = generate_sample(random_state)
+
+plt.imshow(image, cmap="Greys")
+
+print("The target for this image is: {0}".format(target))
+
+
+
+
 
 
